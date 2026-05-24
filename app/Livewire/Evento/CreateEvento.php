8e31b219f2d12cceb416e60dff06edcd513c80e7
promehteo <alejandrofenomeno72@gmail.com>
2026-05-24 10:57:24 -0400

    validacion para ocultar los colores ya seleccionados pero no elimina los colores seleccionados resientemente

--- app/Livewire/Evento/CreateEvento.php
+++ app/Livewire/Evento/CreateEvento.php
@@ -41,6 +41,11 @@ class CreateEvento extends Component
     public function cargarColores()
     {
         $this->colores = \App\Models\Color::where('estatus', '1')
+            ->whereNotIn('id_color', function ($query) {
+                $query->select('id_color')
+                    ->from('evento')
+                    ->whereNotNull('id_color');
+            })
             ->orderBy('nombre_color')
             ->get();
     }
